8c2e4d5cd0947524296dae9bd3ec3f2eca1acf69
jiakaiy <95312547+jiakaiy@users.noreply.github.com>
2023-03-28 14:09:40 -0400

    Update sq.m

--- sq.m
+++ sq.m
@@ -1,4 +1,4 @@
-% +a is translation for time (shifting the time to the right)
+% +a is translation for time (shifting the time to the right), (t-a) means shifts time to the left, everthing happens sooner, which is called a phased event
 function out = sq(t,a)
 x = 0.5;
 if mod(t-a, 24) < 12
