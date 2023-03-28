b8b649b38aa9f01be22edefd5760ab6a6d885988
jiakaiy <95312547+jiakaiy@users.noreply.github.com>
2023-03-28 15:04:30 -0400

    Update p_continuation.m

--- p_continuation.m
+++ p_continuation.m
@@ -1,18 +1,20 @@
 % Oscillatory Enzyme Reactions
 % simple numerical continuation of equilibria
-clear; close all;
+clear; 
+close all;  % clear all variables and close all figures
 
 
 % continuation in p
 n=8;
 m = 20; % number of iterations through p
-alpha1_start = 0; alpha1_end = 1;
-alpha1_vec = linspace(alpha1_start,alpha1_end,m);
+alpha1_start = 0; 
+alpha1_end = 1;
+alpha1_vec = linspace(alpha1_start,alpha1_end,m); % Creates a vector of m equally spaced values of alpha1 between alpha1_start and alpha1_end.
 
-x_vec = zeros(n,m); % initialize matrix of solutions
-lambda = zeros(n,m); % initialize matrix of eigenvalues
-peak=zeros(m); % initialize array of peak
-mins=zeros(m); % initialize array of mins 
+x_vec = zeros(n,m);               % Initializes a matrix of size n x m to store the solutions at each value of alpha1.
+lambda = zeros(n,m);              % Initializes a matrix of size n x m to store the eigenvalues of the Jacobian at each value of alpha1.
+peak=zeros(m);                    %  Initializes an array of size m to store the peak values.
+mins=zeros(m);                    % Initializes an array of size m to store the minimum values.
 
 % initial guess
 x0 = ones(n,1);
@@ -23,7 +25,7 @@ for i=1:m
     [x,fval,exitflag,output,jacobian] = fsolve(@(y) rhs(0,y,alpha1), x0);
     x_vec(:,i) = x; % save equilibrium solution
     x0 = x; % set new x0 for next iteration
-    lambda(:,i) = eig(jacobian); % save eigenvalues of jacobian
+    lambda(:,i) = eig(jacobian); % Stores the eigenvalues of the Jacobian in the i-th column of lambda.
 end
 
 % find index of alpha_1
@@ -32,12 +34,13 @@ index = find(abs(alpha1_vec-0.1579) < 0.001);
 % plot results
 figure;
 subplot(2,1,1);
-plot(alpha1_vec(1:4),x_vec(1,1:4),'-bo','LineWidth',2);
+plot(alpha1_vec(1:4),x_vec(1,1:4),'-bo','LineWidth',2);    %  Plots the first 4 values of alpha1 against the first component of the solution vector x
 hold on;
-plot(alpha1_vec(5:20),x_vec(1,5:20), ':bo', 'LineWidth', 2);
+plot(alpha1_vec(5:20),x_vec(1,5:20), ':bo', 'LineWidth', 2); %  Plots the remaining values of alpha1 against the first component of the solution vector x
 xlabel('$p$','FontSize',18,'Interpreter','latex');
 ylabel('$y_1$','FontSize',18,'Interpreter','latex');
-ax = gca; ax.FontSize = 18;
+ax = gca;
+ax.FontSize = 18;
 hold on;
 
 subplot(2,1,2);
